f2772a7ef6995c37b4eb5af6f0b5f4ef0d92acc2
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-05-06 12:26:28 -0600

    ✨ schemas using zod

---
+++ src/schemas/criptoSchema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const currencySchema = z.object({
+    code: z.string(),
+    name: z.string(),
+})
+
+export const CryptoCurrencySchema = z.object({
+    CoinInfo: z.object({
+        FullName: z.string(),
+        Internal: z.string(),
+    })
+})
+
+export const CryptoCurrenciesSchema = z.array(CryptoCurrencySchema);
