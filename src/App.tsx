66fbb96282091d0f95f293ee40d60c2db855d289
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-05-06 12:28:17 -0600

    ✨ call crypto currencies using store and use effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
+import { useEffect } from "react";
 import CriptoSearchForm from "./components/CriptoSearchForm";
+import { useCriptostore } from "./store";
 
 function App() {
+
+  const { fetchCryptos } = useCriptostore();
+
+  useEffect( ()=>{
+    fetchCryptos();
+  },[fetchCryptos] )
+  
   return (
     <>
       <div className="container">
         <h1 className="app-title">
           Cotizador de <span>Criptomonedas</span>
         </h1>
-        
-          <CriptoSearchForm/>
-        
+
+        <div className="crypto-form-container">
+           <CriptoSearchForm/>
+        </div>
+
       </div>
     </>
   );
