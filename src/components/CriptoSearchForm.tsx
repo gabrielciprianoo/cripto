5c9d119f692a1763754d082d53f3090807d41aa5
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-05-07 17:57:14 -0600

    ✨ validation form and sync state with select values

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,21 +1,55 @@
 
+import { useState, type ChangeEvent, type FormEvent } from 'react';
 import { currencies } from '../data';
 import { useCriptostore } from '../store';
+import type { Pair } from '../types';
 
 export default function CriptoSearchForm() {
 
   const { CryptoCurrencies } = useCriptostore();
+  const [pair, setPair] = useState<Pair>({
+    currency: '',
+    cryptoCurrency: ''
+  });
+
+  const [error, setError] = useState<string>('');
+
+  const handleChange = ( e : ChangeEvent<HTMLSelectElement> ) => {
+    setPair({
+      ...pair,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmmit = ( e : FormEvent<HTMLFormElement> ) => {
+     e.preventDefault();
+
+      if(Object.values(pair).includes('')){
+        setError('Selecciona la moneda y la criptomoneda a convertir');
+        setTimeout(() => {
+          setError('');
+        }, 4000);
+        return;
+      }
+
+
+  }
+
+  
   return (
     <>
       <h2>Buscar Criptomoneda</h2>
-      <form  className="crypto-search-form">
+      <form  className="crypto-search-form" onSubmit={handleSubmmit}>
         <div className="select-container">
           <label htmlFor="currency" className="select-label">
             Seleccione la moneda:
           </label>
           <select
             id="currency"
+            name="currency"
             className="crypto-select"
+            onChange={handleChange}
+            value={pair.currency}
           >
             <option value="">-- Seleccione una moneda --</option>
             {currencies.map((currency) => (
@@ -27,12 +61,15 @@ export default function CriptoSearchForm() {
         </div>
 
         <div className="select-container">
-          <label htmlFor="cryptoType" className="select-label">
+          <label htmlFor="cryptoCurrency" className="select-label">
             Tipo de Criptomoneda:
           </label>
           <select
-            id="cryptoType"
+            id="cryptoCurrency"
+            name="cryptoCurrency"
             className="crypto-select"
+            onChange={handleChange}
+            value={pair.cryptoCurrency}
           >
             <option value="">-- Seleccione el tipo de cryptomoneda --</option>
             {CryptoCurrencies.map(cryptoCurrency => {
@@ -47,6 +84,8 @@ export default function CriptoSearchForm() {
           Buscar
         </button>
       </form>
+
+      { error && <p className='error-message'>{error}</p>}
       
     </>
   );
