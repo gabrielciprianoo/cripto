8ab7a80af5a3564507705d4e5feacc4c41102da6
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-05-08 11:59:55 -0600

    ✨ schema and types for converted currency

--- src/schemas/criptoSchema.ts
+++ src/schemas/criptoSchema.ts
@@ -17,4 +17,14 @@ export const CryptoCurrenciesSchema = z.array(CryptoCurrencySchema);
 export const PairSchema = z.object({
     currency: z.string(),
     cryptoCurrency: z.string(),
+});
+
+export const ConvertedCurrencySchema = z.object({
+    PRICE: z.string(),
+    HIGHDAY: z.string(),
+    LOWDAY: z.string(),
+    IMAGEURL: z.string(),
+    CONVERSIONLASTUPDATE: z.string(),
+    CHANGEPCTDAY: z.string(),
+
 })
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
-import { currencySchema, CryptoCurrencySchema, PairSchema } from "../schemas/criptoSchema";
+import { currencySchema, CryptoCurrencySchema, PairSchema, ConvertedCurrencySchema } from "../schemas/criptoSchema";
 import { z } from "zod";
 
 export type Currency = z.infer<typeof currencySchema>;
 export type CryptoCurrency = z.infer<typeof CryptoCurrencySchema>;
 export type Pair = z.infer<typeof PairSchema>;
+export type ConvertedCurrency = z.infer<typeof ConvertedCurrencySchema>;
